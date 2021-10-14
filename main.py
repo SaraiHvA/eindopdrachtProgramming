f790cf081397c9b2ff55bd3cd7a5032226ea8544
Sarai Misidjan <sarai@conduction.nl>
2021-10-14 04:37:58 +0200

    added class Calculate for calculating the income

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from datetime import date
 
 
-# Program make a simple calculator
+# Calculate gross-net salary
 
 # This class validates the input from the user
 class Validation:
@@ -14,33 +14,42 @@ class Validation:
             datetime.strptime(str(inputBirthday), '%d-%m-%Y')
             return inputBirthday
         except ValueError:
-            print(str(inputBirthday) + f' is geen geldige geboortedatum, geef een geldige datum op, die een d-m-Y formaat heeft.')
+            print(str(inputBirthday) + f' is geen geldige geboortedatum, geef een geldige datum op, die een d-m-Y '
+                                       f'formaat heeft.')
 
     # This function validates the period
     # Expects the input is an item from the given list
-    def validateList(input: str):
-        if input in periodList:
-            return input
-        elif input in fulltimeList:
-            return input
+    def validatePeriodList(inputPeriodList: str):
+        if inputPeriodList in periodList:
+            return inputPeriodList
         else:
-            print(str(input) + f' is geen geldige loonperiode, kies een loonperiode uit de lijst.')
+            print(str(inputPeriodList) + f' is geen geldige loonperiode, kies een loonperiode uit de lijst.')
+
+    # This function validates the full-time hours
+    # Expects the input is an item from the given list
+    def validateFulltimeList(inputFulltimeList: str):
+        fulltimeInt = int(inputFulltimeList)
+        if fulltimeInt in fulltimeList:
+            return fulltimeInt
+        else:
+            print(str(fulltimeInt) + f' is geen geldige loonperiode, kies een loonperiode uit de lijst.')
 
     # This function validates the working hours per week
     # Expect the input to be greater than 0 and less than or equal to 168
-    def validateWorkingHours(input: int):
-        if int(input) and 40 >= int(input) > 0:
-            return input
+    def validateWorkingHours(inputHours: int):
+        if int(inputHours) and 40 >= int(inputHours) > 0:
+            return inputHours
         else:
-            print(str(input) + f' is geen geldige invoer, geef het juiste aantal uren per week op voor een fulltime werkweek.')
+            print(str(inputHours) + f' is geen geldige invoer, geef het juiste aantal uren per week op voor een '
+                                    f'fulltime werkweek.')
 
     # This function validates the hours for a fulltime working week
     # Expect the input to be greater than 0 and less than or equal to the given input hours
-    def validateWorkingHoursWeek(input: int):
-        if int(input) and int(hours) > int(input) > 0:
-            return input
+    def validateWorkingHoursWeek(inputHoursWeek: int):
+        if int(inputHoursWeek) and int(hours) > int(inputHoursWeek) > 0:
+            return inputHoursWeek
         else:
-            print(str(input) + f' is geen geldige invoer, geef het juiste gewerkte uren per week op.')
+            print(str(inputHoursWeek) + f' is geen geldige invoer, geef het juiste gewerkte uren per week op.')
 
 
 def calculate_age(born):
@@ -48,24 +57,74 @@ def calculate_age(born):
     return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
 
 
-# This class calculates the minimum wage
+def calculateIncome(birthday, period, hours, hoursWeek):
+    anwserIncome = Calculate.calculateBirthday(birthday)
+    anwserFulltime = Calculate.calculateFullTimeHours(hours, anwserIncome)
+    # print("anwserFulltime")
+    # print(anwserFulltime)
+    answerHours = Calculate.calculateHours(hoursWeek, hours, anwserFulltime)
+    # print("answerHours")
+    # print(answerHours)
+    answer = Calculate.giveDesiredResult(period, answerHours)
+    return answer
+
 class Calculate:
 
-    def calculateIncome(self, birthday, period, hour, hourWeek):
+    # This class calculates the minimum income for a 40 hour working week
+    def calculateBirthday(birthday):
         if birthday >= 21:
             income = 9.72
+            return income
         elif birthday == 20:
             income = 7.78
+            return income
         elif birthday == 19:
             income = 5.84
+            return income
         elif birthday == 18:
             income = 4.86
+            return income
         elif birthday == 17:
             income = 3.84
+            return income
         elif birthday == 16:
             income = 3.36
+            return income
         elif birthday <= 15:
             income = 2.92
+            return income
+
+    def calculateFullTimeHours(hours, income):
+        # print(hours, income)
+        if(hours == 38):
+            newIncome = income / 5
+            return newIncome
+        elif(hours == 36):
+            newIncome = income / 2
+            return newIncome
+        else:
+            return income
+
+    def calculateHours(hoursWeek, hours, income):
+        # print(income, hours, hoursWeek)
+        newIncome = int(income) / int(hours) * int(hoursWeek)
+        return newIncome
+
+    def giveDesiredResult(period, income):
+        if(period == 'maand'):
+            return income
+        elif(period == 'dag'):
+            newIncome = income / 31
+            return newIncome
+        elif(period == 'week'):
+            newIncome = income / 31 * 7
+            return income
+        elif(period == 'jaar'):
+            newIncome = income / 31 * 365
+            return newIncome
+        return
+
+
 
 
 print("Minimum loon berekenen.")
@@ -82,10 +141,12 @@ while True:
             break
 
     while True:
-        periodList = ["maand", "dag", "week", "4 weken", "kwartaal", "jaar", "alle"]
-        print(', '.join(periodList))
+        # periodList = ["maand", "dag", "week", "4 weken", "kwartaal", "jaar", "alle"]
+        periodList = ["maand", "dag", "week", "jaar"]
+        print(periodList)
+        # print(', '.join(periodList))
         payPeriod = input("Kies de gewenste minimum loon berekening: ")
-        if not Validation.validateList(payPeriod):
+        if not Validation.validatePeriodList(payPeriod):
             continue
         else:
             period = payPeriod
@@ -93,9 +154,10 @@ while True:
 
     while True:
         fulltimeList = [36, 38, 40]
-        print(', '.join(fulltimeList))
+        print(fulltimeList)
+        # print(', '.join(fulltimeList))
         hoursInput = input("Kies een fulltime werkweek in uw branche of sector uit de lijst hierboven: ")
-        if not Validation.validateList(hoursInput):
+        if not Validation.validateFulltimeList(hoursInput):
             continue
         else:
             hours = hoursInput
@@ -109,8 +171,13 @@ while True:
             hoursWeek = hoursWeekInput
             break
 
-    # # check if the input from user is valid
-    # if validateYear(yearInput) and validateSalary(salaryInput) and validateBirthday(birthdayInput):
-    #     print("Valid Input")
-    # else:
-    #     print("Invalid Input")
+    result = calculateIncome(birthday, period, hours, hoursWeek)
+    if result:
+        print("Uw netto " + period + " salaris is: ")
+        print(result)
+
+    tryAgain = input("Wilt u opnieuw een bruto-netto berekening doen?")
+    if tryAgain == 'ja':
+        continue
+    else:
+        break
